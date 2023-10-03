88f3dcd636870c475daefd8f1d57dd9801dc32e7
stpM64 <sssstepa452@yandex.ru>
2023-10-03 12:58:42 +0300

    Now full stplib reload will peform clientside reload as well

--- lua/autorun/stp_00_00_libcore.lua
+++ lua/autorun/stp_00_00_libcore.lua
@@ -4,8 +4,24 @@ local function DoLoad()
 end
 
 DoLoad()
-concommand.Add("stplib_reload_full", function()
+concommand.Add("stplib_reload_full_sv", function()
+    assert(SERVER)
+
     print("==== Full reload begin")
     DoLoad()
+    if SERVER then
+        for _, ply in ipairs(player.GetHumans()) do
+            ply:ConCommand("stplib_reload_full_cl")
+        end
+    end
+
     print("==== Full reload end")
 end)
+
+if CLIENT then
+    concommand.Add("stplib_reload_full_cl", function()
+        print("==== Full reload begin")
+        DoLoad()
+        print("==== Full reload end")
+    end)
+end
