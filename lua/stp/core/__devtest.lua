7c98dfa027f6df72d96391b9223009cd398b2258
stpM64 <sssstepa452@yandex.ru>
2023-10-05 18:16:42 +0300

    Added new __devtest

--- lua/stp/core/__devtest.lua
+++ lua/stp/core/__devtest.lua
@@ -1,17 +1,61 @@
 -- Add test code into this file. Clear this file in release commits.
 
 local libo = stp.obj
+local libn = stp.obj.net
 
 print("---- __devtest.lua start")
 
-local META = libo.BeginTrait("testmeta")
-do
-    print("---", META, META.__index == META, debug.getmetatable(META) == META)
-    PrintTable(META)
-    print("---")
-    print("META.NotExists = ", META.NotExists)
+local META = libo.BeginObject("PerfectCasino.Poker")
+libo.ApplyMany(META,
+    libn.Instantiatable,
+    libo.VariableContainer
+)
+
+libo.ConstructNestedType(META, "Bank",
+    libo.MakeVariableField,
+    libo.MakeVariableAccessors(
+        "GetBank",
+        SERVER and "SetBank",
+        "OnBankChanged"
+    ),
+
+    CLIENT and libo.VariableRequireInit(),
+    SERVER and libo.VariableDefault(0),
+
+    libn.MakeVar(libn.schema.UInt(16)),
+    libn.MakeRecipientEveryone,
+    libn.MakeReliable
+)
+
+if SERVER then
+    function META:NetGetRecipients(recip)
+        recip:AddAllPlayers()
+    end
+
+    function META:NetTransmitInit()
+        net.WriteUInt(self:GetBank(), 16)
+    end
+else
+    function META.NetReceiveInit()
+        local bank = net.ReadUInt(16)
+        return { Bank = bank }
+    end
+end
+
+
+function META:OnBankChanged(old, new)
+    print(self, "New bank value = ", new)
 end
 
 libo.Register(META)
+--Poker.Game = META
 
+
+function CreateGame(ent_table)
+    local obj = META.Create({
+        Table = ent_table
+    })
+
+    return obj
+end
 print("---- __devtest.lua end")
