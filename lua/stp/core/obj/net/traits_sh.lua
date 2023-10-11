8bbf2c7da3ea6406bcf0aadff62e44567ac7d3c2
stpM64 <sssstepa452@yandex.ru>
2023-10-12 00:29:58 +0300

    Added TODO to call NetSetRestrictor

--- lua/stp/core/obj/net/traits_sh.lua
+++ lua/stp/core/obj/net/traits_sh.lua
@@ -17,7 +17,7 @@ function NETABLE:NetGetRestrictor()
     return self.__net_restrictor
 end
 
-function NETABLE:NetSetRestrictor(restrictor)
+function NETABLE:NetSetRestrictor(restrictor) -- TODO: call this for variables
     libn.restrictors._Set(self, restrictor)
     self.__net_restrictor = restrictor
 end
