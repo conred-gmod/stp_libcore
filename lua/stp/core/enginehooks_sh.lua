93af6da274be4546f88f65afa772b3be3702e010
stpM64 <sssstepa452@yandex.ru>
2024-03-18 13:46:25 +0300

    Minor changes to NetReady tracking

--- lua/stp/core/enginehooks_sh.lua
+++ lua/stp/core/enginehooks_sh.lua
@@ -1,25 +1,19 @@
 local libn = stp.obj.net 
 
+-- Add a signle hook handler here if specific order of handler execution is necessary:
+--[[
+    hook.Add("OnExample", "stp.DoAllStuff", function()
+        stp.module1._OnExampleHandler()
+        stp.module2._OnExampleHandler() -- Depends on results of previous handler's actions
+        stp.module3._OnExampleHandler() -- Depends on results of previous handlers' actions
+    end)
+]] 
+--
+-- Do not add a handler for a single module, do that in module's file (or folder) instead.
+
 hook.Add("Tick", "stp.DoAllStuff", function()
     if SERVER then
         libn.awareness._Update()
     end
     libn._TransmitAll()
-end)
-
-local queue = {}
-
-hook.Add("PlayerInitialSpawn", "stp.NetReady", function(ply)
-    queue[ply:UserID()] = true
-end)
-
-gameevent.Listen("OnRequestFullUpdate")
-hook.Add("OnRequestFullUpdate", "stp.NetReady", function(data)
-    local userid = data.userid
-    
-    if queue[userid] == nil then return end
-    queue[userid] = nil
-
-    local ply = Player(userid)
-    ply.stp_NetReady = true
 end)
